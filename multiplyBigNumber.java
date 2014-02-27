0a077207dc41b81313a888afcfd92d510c106243
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-27 22:44:16 +0800

    Updated multiplyBigNumber binding procedure.

--- multiplyBigNumber.java
+++ multiplyBigNumber.java
@@ -2,8 +2,8 @@ import java.math.BigInteger;
 public class multiplyBigNumber{
 	public static void main( String... numbers ){
 		try{
-			BigInteger number = BigInteger.ONE;
-			for( int index = 0; index < numbers.length; index++ ){
+			BigInteger number = new BigInteger( numbers[ 0 ] );
+			for( int index = 1; index < numbers.length; index++ ){
 				number = number.multiply( new BigInteger( numbers[ index ] ) );	
 			}
 			System.out.print( number.toString( ) );
