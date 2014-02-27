537fd11ecd61a151057a06c7c3bf9004a40e7b44
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-27 22:42:22 +0800

    Updated multiplyBigNumber binding and procedures.

--- multiply-big-number.js
+++ multiply-big-number.js
@@ -1,17 +1,104 @@
-/*:
-	@include:
-		{
-			"work": "work"
-		}
-	@end-include
-*/
-multiplyBigNumber = function multiplyBigNumber( numbers, callback ){
-	/*:
-		@meta-configuration:
-			{
-				"numbers": "string|number..."
+try{
+	var environment = process.env;
+	environment = "nodejs";
+}catch( error ){
+	var environment = "browser";
+}
+try{
+	var base = window;
+	environment = "node-webkit";
+}catch( error ){
+	var base = exports;
+}
+( function module( base ){
+	try{
+		var moduleDefine = define; 
+	}catch( error ){
+		if( environment == "node-webkit" ){
+			//It means we can auto reload the requirejs module.
+			var fs = require( "fs" );
+			var libraryPath = process.cwd( ) + "/library/".replace( "/", path.sep );
+			if( fs.existsSync( libraryPath ) ){
+				//TODO: Find the script tag containing the requirejs
+				var scriptReferenceNode = document.getElementsByTagName( "head" );
+				var requirejsScriptLoaderNode = document.createElement( "script" );
+				requirejsScriptLoaderNode.setAttribute( "type", "text/javascript" );
+				requirejsScriptLoaderNode.setAttribute( "src",
+					libraryPath + "require/require.js" );
+				scriptReferenceNode.parentNode.insertBefore( requirejsScriptLoaderNode, scriptReferenceNode );
 			}
-		@end-meta-configuration
+		}
+		//TODO: Make this work for nodejs and browser environment.
+		var moduleDefine = function define( moduleName, dependencies, moduleConstruct ){
+			var parameters = Array.prototype.slice.apply( arguments );
+			if( typeof parameters[ 1 ] == "function" ){
+				moduleConstruct = parameters[ 1 ];
+			}
+			moduleConstruct( );
+		}
+	}
+	//TODO: Put the main construction for the module here
+	/*:
+		@required-environment:
+			[
+				"nodejs",
+				"node-webkit"
+			]
+		@end-required-environment
 	*/
-	work( "javac multiplyBigNumber.java && java multiplyBigNumber " + numbers.join( " " ), callback );
-};
+	moduleDefine( "multiplyBigNumber",
+		function construct( ){
+			var childprocess = require( "child_process" );
+			var fs = require( "fs" );
+			var path = require( "path" );
+			var pathDirectory = process.cwd( ) + "/multiply-big-number/".replace( "/", path.sep );
+			var javaClassFile = pathDirectory + "multiplyBigNumber.class";
+			var isJavaClassFileExist = fs.existsSync( javaClassFile );
+
+			var multiplyBigNumber = function multiplyBigNumber( numbers, callback ){
+				var parameters = Array.prototype.slice.apply( arguments );
+				callback = parameters.pop( );
+				if( typeof callback != "function" ){
+					throw new Error( "invalid parameter" );
+				}
+				numbers = parameters;
+				for( var index = 0; index < numbers.length; index++ ){
+					var number = numbers[ index ];
+					if( typeof number != "string"
+						|| typeof number != "number" )
+					{
+						throw new Error( "invalid parameter" );
+					}
+				}
+				var command = "java " + pathDirectory + "multiplyBigNumber " + numbers.join( " " );	
+				if( !isJavaClassFileExist ){
+					command = "javac " + pathDirectory + "multiplyBigNumber.java && " + command;
+				}
+				var task = childprocess.exec( command );
+				var error = "";
+				var output = "";
+				task.stdout.on( "data",
+					function( data ){
+						output += data.toString( );
+					} );
+				task.stderr.on( "data",
+					function( data ){
+						error += data.toString( ).replace( /^\s+|\s+$/g, "" );
+					} );
+				task.on( "close",
+					function( ){
+						if( error ){
+							error = new Error( error );
+							callback( error );
+						}else{
+							/*
+								TODO: Add validation here that 
+								will be returned as second parameter.
+							*/
+							callback( null, true, output );
+						}
+					} );
+			};
+			base.multiplyBigNumber = multiplyBigNumber;
+		} );
+} )( base );
